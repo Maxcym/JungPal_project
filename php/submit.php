d14d96e5ec7780886efd2009fb4d6232bf7ab277
maxime <maxime.cayman@isen-ouest.yncrea.fr>
2024-07-11 22:52:57 +0200

    Code review

--- php/submit.php
+++ php/submit.php
@@ -1,17 +1,38 @@
 <?php
 include("bdd.php");
 
-$ad_id = isset($_POST['ad_id']) ? $_POST['ad_id'] : null;
-$user_id = $_POST['user_id'];
-$party = $_POST['party'];
-$garden = $_POST['garden'];
-$cleaning = $_POST['cleaning'];
-$rooms = $_POST['rooms'];
-$price = $_POST['price'];
-$size = $_POST['size'];
-$internet = $_POST['internet'];
-$deposit = $_POST['deposit'];
-$campus_time = $_POST['campus-time'];
+// Fonction de débogage
+function debug_to_console($data) {
+    $output = $data;
+    if (is_array($output))
+        $output = implode(',', $output);
+
+    echo "<script>console.log('Debug: " . $output . "' );</script>";
+}
+
+$ad_id = isset($_POST['id']) ? $_POST['id'] : null;
+$user_id = isset($_POST['user_id']) ? $_POST['user_id'] : null;
+$party = isset($_POST['party']) ? $_POST['party'] : null;
+$garden = isset($_POST['garden']) ? $_POST['garden'] : null;
+$cleaning = isset($_POST['cleaning']) ? $_POST['cleaning'] : null;
+$rooms = isset($_POST['rooms']) ? $_POST['rooms'] : null;
+$price = isset($_POST['price']) ? $_POST['price'] : null;
+$size = isset($_POST['size']) ? $_POST['size'] : null;
+$internet = isset($_POST['internet']) ? $_POST['internet'] : null;
+$deposit = isset($_POST['deposit']) ? $_POST['deposit'] : null;
+$campus_time = isset($_POST['campus-time']) ? $_POST['campus-time'] : null;
+
+// Log des valeurs avant la requête
+debug_to_console("user_id: $user_id");
+debug_to_console("party: $party");
+debug_to_console("garden: $garden");
+debug_to_console("cleaning: $cleaning");
+debug_to_console("rooms: $rooms");
+debug_to_console("price: $price");
+debug_to_console("size: $size");
+debug_to_console("internet: $internet");
+debug_to_console("deposit: $deposit");
+debug_to_console("campus_time: $campus_time");
 
 if ($ad_id) {
     // Update existing ad
