0c68f6f0fe85255ed043c085f6a3a7321a9a51c0
Marylinelb2105 <marylinelebot21@gmail.com>
2024-07-11 17:22:21 +0200

    sql

--- jungpal.sql
+++ jungpal.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : mer. 10 juil. 2024 à 08:37
+-- Généré le : jeu. 11 juil. 2024 à 17:21
 -- Version du serveur : 10.4.32-MariaDB
 -- Version de PHP : 8.2.12
 
@@ -47,16 +47,9 @@ CREATE TABLE `ads` (
 --
 
 INSERT INTO `ads` (`id`, `party`, `garden`, `cleaning`, `rooms`, `price`, `size`, `internet`, `deposit`, `campus_time`, `visible`, `user_id`) VALUES
-(8, 'no', 'no', 'no', 2, 300.00, 10.00, 'yes', 600.00, 2, 0, 2),
-(9, 'yes', 'no', 'no', 2, 350.00, 12.00, 'yes', 700.00, 2, 0, 3),
-(10, 'yes', 'no', 'no', 1, 400.00, 11.00, 'yes', 800.00, 2, 0, 4),
-(11, 'yes', 'no', 'no', 2, 300.00, 10.00, 'yes', 600.00, 2, 0, 6),
-(12, 'yes', 'yes', 'no', 2, 250.00, 10.00, 'yes', 500.00, 4, 0, 5),
-(13, 'yes', 'no', 'no', 2, 300.00, 16.00, 'yes', 600.00, 4, 0, 7),
-(14, 'yes', 'no', 'no', 2, 350.00, 14.00, 'yes', 700.00, 4, 0, 8),
-(15, 'yes', 'no', 'no', 2, 300.00, 10.00, 'yes', 600.00, 4, 0, 9),
 (32, 'no', 'no', 'no', 4, 340.00, 14.00, 'yes', 680.00, 42, 1, 12),
-(33, 'yes', 'yes', 'yes', 3, 300.00, 10.00, 'no', 600.00, 0, 1, 13);
+(33, 'yes', 'yes', 'yes', 3, 300.00, 10.00, 'no', 600.00, 0, 1, 13),
+(35, 'yes', 'no', 'no', 3, 320.00, 15.00, 'yes', 640.00, 18, 1, 14);
 
 -- --------------------------------------------------------
 
@@ -82,19 +75,9 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `surname`, `gender`, `dob`, `address`, `city`, `postal_code`, `email`, `password`) VALUES
-(1, 'Le Bot', 'Maryline', 'woman', '2002-05-21', '29, langeoguer', 'Plougar', '29440', 'marylinelebot21@gmail.com', 'lb21'),
-(2, 'Le Bot', 'Maryline2', 'woman', '2002-05-21', '29, langeoguer', 'Plougar', '29440', 'marylinelebot22@gmail.com', 'lb21'),
-(3, 'Maryline', 'Le Bot', 'woman', '2024-06-13', '29, langeoguer', 'Plougar - (29440)', '29440', 'marylinelebot23@gmail.com', 'lb'),
-(4, 'Maryline', 'Le Bot', 'woman', '2002-05-21', '29, langeoguer', 'Plougar', '29440', 'test@ufyyu', 'lb'),
-(5, '', '', 'woman', '2002-05-21', '29, langeoguer', 'Plougar - (29440)', '29440', 'marylinelebot24@gmail.com', 'lb'),
-(6, 'bla', 'bla', 'bla', '4567-03-12', 'bla', 'bl', 'bla', 'bla@hfehef', 'bla'),
-(7, 'Maryline', 'Le Bot', 'woman', '2002-05-21', '29, langeoguer', 'Plougar - (29440)', '29440', 'jhqfoiheol@ikbzd', '1'),
-(8, 'Maryline', 'Le Bot', 'woman', '2002-05-21', '29, langeoguer', 'Plougar - (29440)', '29440', 'kugjazazgoaz@kvjzdkj', '2'),
-(9, 'Maryline', 'Le Bot', 'woman', '2002-05-21', '29, langeoguer', 'Plougar - (29440)', '29440', 'efbfuke@ekdjf', 'l'),
-(10, '', '', '', '0000-00-00', '', '', '', '', ''),
-(11, 'dgs', 'iah', 'oirro', '4566-03-12', 'akejefn', 'ljken', 'lnkae', 'alnf@]jlknf', '1'),
-(12, 'Léon', 'Goretzka', 'Male', '1995-02-06', 'Allianz Arena', 'Munich', '84000', 'leon.goretzka@gmail.com', '12'),
-(13, 'jo', 'kimmich', 'male', '1995-02-06', 'allianz arena', 'munich', '8400', 'joshua.kimmich@gmail.com', '06');
+(12, 'Léon', 'Goretzka', 'Male', '1995-02-06', 'Allianz Arena', 'Munich', '80', 'leon.goretzka@gmail.com', '08'),
+(13, 'Joshua', 'Kimmich', 'Male', '1995-02-06', 'Allianz Arena', 'munich', '80', 'joshua.kimmich@gmail.com', '06'),
+(14, 'Manuel', 'Neuer', 'Male', '1986-03-27', 'Allianz Arena', 'Munich', '80', 'manuel.neuer@gmail.com', '01');
 
 --
 -- Index pour les tables déchargées
@@ -120,13 +103,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT pour la table `ads`
 --
 ALTER TABLE `ads`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
 
 --
 -- AUTO_INCREMENT pour la table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(6) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(6) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
